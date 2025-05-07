894f2b0b387d8e98915561af6349a000f7c5bfa3
KING-MOM <67985327+KING-MOM@users.noreply.github.com>
2025-05-06 21:23:48 -0400

    Update tools.js

--- api/tools.js
+++ api/tools.js
@@ -2,28 +2,35 @@ export default function handler(req, res) {
   res.status(200).json({
     tools: [
       {
-        name: "list_assistants",
-        description: "Returns a list of assistants configured in the system.",
-        parameters: {
-          type: "object",
-          properties: {},
-          required: []
+        type: "function",
+        function: {
+          name: "list_assistants",
+          description: "Returns a list of assistants.",
+          parameters: {
+            type: "object",
+            properties: {},
+            required: []
+          }
         }
       },
       {
-        name: "get_weather",
-        description: "Returns the weather for a given city.",
-        parameters: {
-          type: "object",
-          properties: {
-            city: {
-              type: "string",
-              description: "The name of the city"
-            }
-          },
-          required: ["city"]
+        type: "function",
+        function: {
+          name: "get_weather",
+          description: "Returns the weather for a given city.",
+          parameters: {
+            type: "object",
+            properties: {
+              city: {
+                type: "string",
+                description: "The name of the city"
+              }
+            },
+            required: ["city"]
+          }
         }
       }
     ]
   });
 }
+
